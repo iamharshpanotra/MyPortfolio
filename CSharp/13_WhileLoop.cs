3b55f4acf404da8cec053894abb89130bc84b774
Harsh <harsh.sharma@maxworthsystems.com>
2025-01-27 15:22:16 +0530

    Initial Commit

---
+++ CSharp/13_WhileLoop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp
+{
+    class _13_WhileLoop
+    {
+        public static void While()
+        {
+            Console.WriteLine("Please enter your choice");
+            int choice = int.Parse(Console.ReadLine());
+
+            int cost = 0;
+
+            while (cost <= choice)
+            {
+                Console.Write(cost + " ");
+                cost = cost + 2;
+            }
+
+            Console.ReadLine(); // To keep the console window open after execution
+        }
+
+        // Main method to start the execution
+        //static void Main(string[] args)
+        //{
+        //    // Call the While method here to run the program
+        //    While();
+        //}
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //_13_WhileLoop.While();
+            _14_DoWhileLoop.While();
+        }
+    }
+}
